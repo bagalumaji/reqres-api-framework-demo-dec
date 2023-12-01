def0a049480b946cdb26ff3b2d0e041eea0d6f45
bagalumaji <bagalumaji@gmail.com>
2023-12-01 08:56:32 -0800

    created user test class and added 1 method to get a single user

---
+++ src/test/java/com/bagal/tests/user/UserTests.java
@@ -0,0 +1,20 @@
+package com.bagal.tests.user;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+public class UserTests {
+
+    @Test
+    public void getSingleUserTest(){
+        Response response = RestAssured.get("https://reqres.in/api/users/2");
+        int statusCode = response.statusCode();
+
+        System.out.println("status code : "+statusCode);
+        System.out.println("status Line ==> "+response.statusLine());
+        System.out.println(response.header("content-Type"));
+        System.out.println("response.header(\"content-Type\") = " + response.header("content-Type"));
+        response.prettyPrint();
+    }
+}
