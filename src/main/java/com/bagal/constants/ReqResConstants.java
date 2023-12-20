11f6b7f31f35d5f83a0ed38411f8c8748b840bd1
bagalumaji <bagalumaji@gmail.com>
2023-12-20 07:57:29 +0530

    added constant for propertyfile location

--- src/main/java/com/bagal/constants/ReqResConstants.java
+++ src/main/java/com/bagal/constants/ReqResConstants.java
@@ -1,4 +1,10 @@
 package com.bagal.constants;
 
-public class ReqResConstants {
+import lombok.Getter;
+
+public final class ReqResConstants {
+    private ReqResConstants(){}
+
+    private static final String BASE_PATH=System.getProperty("user.dir");
+    private @Getter static final String APPLICATION_PROPERTY_FILE_LOCATION = BASE_PATH+"/src/test/resources/configs/application.properties";
 }
