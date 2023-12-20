9db91318f3300e2797ea67257fc168e8e39c883e
bagalumaji <bagalumaji@gmail.com>
2023-12-20 07:56:59 +0530

    created exception for property key

---
+++ src/main/java/com/bagal/exceptions/PropertyKeyNotFoundException.java
@@ -0,0 +1,7 @@
+package com.bagal.exceptions;
+
+public class PropertyKeyNotFoundException extends RuntimeException {
+    public PropertyKeyNotFoundException(String message) {
+        super(message);
+    }
+}
