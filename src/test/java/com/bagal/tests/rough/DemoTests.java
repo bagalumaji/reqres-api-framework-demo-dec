96c43f1bbfce3bb12707bac4caf9dac3dd6c1ccf
bagalumaji <bagalumaji@gmail.com>
2024-01-02 19:44:40 +0530

    demo test

--- src/test/java/com/bagal/tests/rough/DemoTests.java
+++ src/test/java/com/bagal/tests/rough/DemoTests.java
@@ -5,8 +5,12 @@ import com.bagal.models.response.user.CreateUserResponsePojo;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.http.ContentType;
+import io.restassured.module.jsv.JsonSchemaValidator;
 import io.restassured.response.Response;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.io.File;
 
 import static com.bagal.configs.PropertyConfig.getValue;
 import static io.restassured.RestAssured.given;
@@ -14,7 +18,8 @@ import static io.restassured.RestAssured.given;
 public class DemoTests {
     @Test
     public void demoTest() throws JsonProcessingException {
-        CreateUserRequestPoJo userPoJo = CreateUserRequestPoJo.builder().name("sayaji").job("dev").build();
+        CreateUserRequestPoJo userPoJo = CreateUserRequestPoJo.builder().
+                setName("sayaji").setJob("dev").build();
         ObjectMapper mapper = new ObjectMapper();
         String body = mapper.writeValueAsString(userPoJo);
         Response response = given()
@@ -25,11 +30,32 @@ public class DemoTests {
                 .when()
                 .post();
         String resBody = response.body().asString();
+
         CreateUserResponsePojo userResponsePojo = mapper.readValue(resBody, CreateUserResponsePojo.class);
         System.out.println("userResponsePojo.getCreatedAt() = " + userResponsePojo.getCreatedAt());
         System.out.println("userResponsePojo.getId() = " + userResponsePojo.getId());
         System.out.println("userResponsePojo.getName() = " + userResponsePojo.getName());
         System.out.println("userResponsePojo.getJob() = " + userResponsePojo.getJob());
+        response.then().body(JsonSchemaValidator.matchesJsonSchema(new File("/Users/sayajirajebagal/repos/reqres-api-framework-demo-dec/src/test/resources/schema/CreateUserSchema.json")));
     }
 
+    @Test(dataProvider = "getData")
+    public void readJsonTest(Object[] data) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        CreateUserRequestPoJo createUserRequestPoJo = mapper.readValue(data[0].toString(), CreateUserRequestPoJo.class);
+        System.out.println("createUserRequestPoJo.getName() = " + createUserRequestPoJo.getName());
+        System.out.println("createUserRequestPoJo.getJob() = " + createUserRequestPoJo.getJob());
+
+    }
+
+    @DataProvider
+    public Object[] getData() throws JsonProcessingException {
+
+        CreateUserRequestPoJo userPoJo = CreateUserRequestPoJo.builder().setName("sayaji").setJob("dev").build();
+        Object[] data = new Object[1];
+        ObjectMapper mapper = new ObjectMapper();
+        String createUserRequestPoJo = mapper.writeValueAsString(userPoJo);
+        data[0] = createUserRequestPoJo;
+        return data;
+    }
 }
