1cc4a754584d2afe3d72421d39cb385ac797b20c
bagalumaji <bagalumaji@gmail.com>
2024-01-02 19:44:17 +0530

    create user request pojo

--- src/main/java/com/bagal/models/request/user/CreateUserRequestPoJo.java
+++ src/main/java/com/bagal/models/request/user/CreateUserRequestPoJo.java
@@ -1,13 +1,75 @@
 package com.bagal.models.request.user;
 
-import lombok.*;
-
-@Getter
-@Setter
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
 public class CreateUserRequestPoJo {
     private String name;
     private String job;
+
+    public CreateUserRequestPoJo() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public CreateUserRequestPoJo setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public CreateUserRequestPoJo setJob(String job) {
+        this.job = job;
+        return this;
+    }
+
+    public static CreateUserRequestBuilder builder() {
+        return new CreateUserRequestBuilder();
+    }
+
+    public CreateUserRequestPoJo(String name, String job) {
+        this.name = name;
+        this.job = job;
+    }
+
+    public static class CreateUserRequestBuilder {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public CreateUserRequestBuilder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public String getJob() {
+            return job;
+        }
+
+        public CreateUserRequestBuilder setJob(String job) {
+            this.job = job;
+            return this;
+        }
+
+        private String job;
+
+        public CreateUserRequestBuilder(String name, String job) {
+            this.name = name;
+            this.job = job;
+        }
+        public CreateUserRequestBuilder(){};
+
+        public CreateUserRequestPoJo build() {
+            return new CreateUserRequestPoJo(name,job);
+        }
+
+
+
+    }
+
+
 }
