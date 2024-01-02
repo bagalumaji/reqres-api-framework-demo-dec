934d6ac9d1d1c1f9455a5ee1b499865ac14e5f89
bagalumaji <bagalumaji@gmail.com>
2024-01-02 19:44:24 +0530

    create user response pojo

--- src/main/java/com/bagal/models/response/user/CreateUserResponsePojo.java
+++ src/main/java/com/bagal/models/response/user/CreateUserResponsePojo.java
@@ -1,15 +1,54 @@
 package com.bagal.models.response.user;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Getter
-@Setter
-@NoArgsConstructor
 public class CreateUserResponsePojo {
     private String  name;
     private String   job;
     private String   id;
     private String createdAt;
+
+    public CreateUserResponsePojo(String name, String job, String id, String createdAt) {
+        this.name = name;
+        this.job = job;
+        this.id = id;
+        this.createdAt = createdAt;
+    }
+
+    public CreateUserResponsePojo() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public CreateUserResponsePojo setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public CreateUserResponsePojo setJob(String job) {
+        this.job = job;
+        return this;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public CreateUserResponsePojo setId(String id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public CreateUserResponsePojo setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+        return this;
+    }
 }
