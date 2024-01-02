6960859cf8d91d97649d25ed8de81618fb2d2d01
bagalumaji <bagalumaji@gmail.com>
2024-01-02 19:48:55 +0530

    ReqResListener

--- src/main/java/com/bagal/listeners/ReqResListener.java
+++ src/main/java/com/bagal/listeners/ReqResListener.java
@@ -1,4 +1,26 @@
 package com.bagal.listeners;
 
-public class ReqResListener {
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class ReqResListener implements ITestListener {
+    @Override
+    public void onTestStart(ITestResult result) {
+        ITestListener.super.onTestStart(result);
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        ITestListener.super.onTestSuccess(result);
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        ITestListener.super.onTestFailure(result);
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        ITestListener.super.onTestSkipped(result);
+    }
 }
